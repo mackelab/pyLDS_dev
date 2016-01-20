Debugged M-step for emission distribution-part. Added "correct" ML solution formula to Regression, debugging mostly concerned the test script currently given by dev/pylds_dev.ipynb. E-step also looks good now (code has been good before, I just forgot to fix the initial distribution parameters manually). To do: have a look at AutoRegression, to introduce "correct" ML equations
Note: The "simple" equations used by Matt agree with the "correct" ML results
      almost up to numerical precision. Will need to finally get out where
      they actually come from.

pylds/distributions.py
@@ -76,13 +76,32 @@ class Regression_diag(Regression):
 
         if n > 0:
             try:
-                self.A = np.linalg.solve(xxT, yxT.T).T
-                self.sigma = (yyT - self.A.dot(yxT.T))/n
-
                 def symmetrize(A):
                     return (A + A.T)/2.
-                self.sigma = 1e-10*np.eye(self.D_out) \
-                    + symmetrize(self.sigma)  # numerical
+
+                if self.affine:
+
+                    yxT, y = yxT[:,:-1], yxT[:,-1]
+                    xxT, x = xxT[:-1, :-1], xxT[:-1, -1]
+
+                    A = np.linalg.solve(xxT - np.outer(x,x)/n, (yxT - np.outer(y,x)/n).T).T
+                    b = np.atleast_2d(y - A.dot(x)).T/n
+                    self.A = np.hstack([A,b])
+
+                    self.sigma = (yyT \
+                                - 2 * symmetrize(np.outer(y, b)) \
+                                - 2 * symmetrize(A.dot(yxT.T - np.outer(x,b))) \
+                                + A.dot(xxT.dot(A.T)) )/n \
+                                + np.outer(b,b)
+
+                else:                    
+
+                    self.A = np.linalg.solve(xxT, yxT.T).T
+
+                    self.sigma = (yyT - self.A.dot(yxT.T))/n
+                    self.sigma = 1e-10*np.eye(self.D_out) \
+                        + symmetrize(self.sigma)  # numerical
+
             except np.linalg.LinAlgError:
                 self.broken = True
         else:

pylds/states.py
@@ -197,7 +197,8 @@ class LDSStates(object):
 
         if self.model.emission_distn.affine:
             Ex, Ey = smoothed_mus.sum(0), data.sum(0)
-            ExxT = blockarray([[ExxT,np.atleast_2d(Ex).T],[np.atleast_2d(Ex),np.atleast_2d(self.T)]])
+            ExxT = blockarray([[ExxT,np.atleast_2d(Ex).T],
+                               [np.atleast_2d(Ex),np.atleast_2d(self.T)]])
             EyxT = np.hstack((EyxT,np.atleast_2d(Ey).T))        
 
         self.E_emission_stats = np.array([EyyT, EyxT, ExxT, self.T])